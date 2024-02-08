199d513a20c1623f30c7bbe81cd44c27eee492d8
Mariela-t <tinoco.mariela01@gmail.com>
2024-02-07 19:29:01 -0800

    Added devise, created recipe model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :recipes
+  root 'recipes#index'
 end
