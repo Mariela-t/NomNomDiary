1405e77d592eb3cb81e152e278633f09c161af61
Mariela-t <tinoco.mariela01@gmail.com>
2024-02-25 23:56:43 -0800

    added home, changed root and styled app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get 'recipes/index'
   get 'recipes/show'
   get 'recipes/new'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :recipes
-  root 'recipes#index'
+  root 'home#index'
+  # root 'recipes#index'
   #  root to: redirect('/users/sign_up')
 end
